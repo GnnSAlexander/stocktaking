7d6ecc2a7a903eadf477ac2961a1938e8ba531e4
GnnSAlexander <gcarrasquillae@gmail.com>
2021-09-14 22:31:31 -0500

    Added tailwind

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="h-20 w-full bg-blue-100">
+        <h1>Stocktaking</h1>
+    </div>
+</body>
+</html>
